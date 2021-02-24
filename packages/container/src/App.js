0e6b14e20ad77be1bfd9deabbe12f93dcba478d5
Ian Tennyson <ian.tennyson@tvsquared.com>
2021-02-24 19:50:46 +0000

    force change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>CONTAINER!</h1>
+    <h1>CONTAINER APP!</h1>
     <hr />
     <MarketingApp />
   </div>
